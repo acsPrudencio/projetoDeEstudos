40fc74bea4378992e1df764f143ae33b95f84f72
Antonio Carlos Soares Prudencio <acsprudencio@gmail.com>
2023-02-28 10:37:22 -0300

    initial config

---
+++ src/main/java/com/estudo/projetodeestudo/ProjetoDeEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.projetodeestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeEstudoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoDeEstudoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estudo/projetodeestudo/ProjetoDeEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.projetodeestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeEstudoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
